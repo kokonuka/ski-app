ec578db226a599489512e343a90b97c57687d3fb
sun-njp <miuraasahi1@gmail.com>
2023-06-19 09:58:14 +0900

    headをコンポーネント化

---
+++ frontend/src/components/Head.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import NextHead from "next/head";
+
+type Props = {
+  title: string;
+  description: string;
+};
+
+export const Head: React.FC<Props> = ({ title, description }) => {
+  return (
+    <NextHead>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </NextHead>
+  );
+};
