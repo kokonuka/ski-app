fd9705855f323cfec82b8adc5f0fec739efb912f
sun-njp <miuraasahi1@gmail.com>
2023-06-19 10:02:52 +0900

    環境変数の追加

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,7 +3,10 @@ import { Head } from "@/components/Head";
 export default function Home() {
   return (
     <>
-      <Head title="App Name" description="app description" />
+      <Head
+        title={`${process.env.NEXT_PUBLIC_TITLE}`}
+        description={`${process.env.NEXT_PUBLIC_DESCRIPTION}`}
+      />
     </>
   );
 }
