7ec983caded082dc186191adcdc7e74193346f35
sun-njp <miuraasahi1@gmail.com>
2023-07-16 17:15:57 +0900

    API Gatewayコンストラクトを作成

---
+++ backend/lib/construct/api/index.ts
@@ -0,0 +1,52 @@
+import { aws_apigateway as apigateway } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Lambda } from "../lambda";
+import { Auth } from "./auth";
+import { Users } from "./users";
+import { GatewayResponse } from "./gatewayResponse";
+
+export interface ApiProps {
+  lambda: Lambda;
+}
+
+export class Api extends Construct {
+  constructor(scope: Construct, id: string, props: ApiProps) {
+    super(scope, id);
+
+    const api = new apigateway.RestApi(this, "SkiAppApi", {
+      deployOptions: {
+        stageName: "v1",
+      },
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+        allowHeaders: apigateway.Cors.DEFAULT_HEADERS,
+        statusCode: 200,
+      },
+    });
+
+    new GatewayResponse(this, "GatewayResponse", { api });
+
+    const auth = new Auth(this, "Auth", {
+      handler: props.lambda.authorizer,
+    });
+
+    const users = new Users(this, "Users", {
+      api,
+      userHandlers: props.lambda.users,
+      authorizer: auth.authorizer,
+    });
+
+    const checkout = api.root.addResource("checkout");
+    checkout.addMethod(
+      "GET",
+      new apigateway.LambdaIntegration(props.lambda.payment.checkout)
+    );
+
+    const webhook = api.root.addResource("webhook");
+    webhook.addMethod(
+      "POST",
+      new apigateway.LambdaIntegration(props.lambda.payment.webhook)
+    );
+  }
+}
---
+++ backend/lib/construct/api/users.ts
@@ -0,0 +1,40 @@
+import { aws_apigateway as apigateway } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { UserHandlers } from "../lambda";
+
+export interface UsersProps {
+  api: apigateway.RestApi;
+  userHandlers: UserHandlers;
+  authorizer: apigateway.TokenAuthorizer;
+}
+
+export class Users extends Construct {
+  readonly authorizer: apigateway.TokenAuthorizer;
+
+  constructor(scope: Construct, id: string, props: UsersProps) {
+    super(scope, id);
+
+    const users = props.api.root.addResource("users");
+
+    /** GET users */
+    users.addMethod(
+      "GET",
+      new apigateway.LambdaIntegration(props.userHandlers.getUsers)
+    );
+
+    /** GET users/{id} */
+    const user = users.addResource("{id}");
+    user.addMethod(
+      "GET",
+      new apigateway.LambdaIntegration(props.userHandlers.getUser),
+      { authorizer: props.authorizer }
+    );
+
+    /** POST users */
+    users.addMethod(
+      "POST",
+      new apigateway.LambdaIntegration(props.userHandlers.createUser),
+      { authorizer: props.authorizer }
+    );
+  }
+}
